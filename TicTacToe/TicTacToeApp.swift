525fd1bb4a993629a24b3b09b4f1a37ef4fa450e
Aiyetin Roy <aiyetinroy@gmail.com>
2022-05-18 13:17:10 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Roy Aiyetin on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
